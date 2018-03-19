6191a4f5346708f3cc65a6a5d042fb76acebf1b3
xiaosuda <xiaosuda@2dfire.com>
2018-03-19 20:35:15 +0800

    socket前端

--- chess/src/main/resources/static/js/socket.js
+++ chess/src/main/resources/static/js/socket.js
@@ -1,10 +1,33 @@
 var Socket = function () {
-    var oChessGame = new Object();
-    oChessGame.send = function (x) {
-
+    var oSocket = new Object();
+    var socket;
+    oSocket.send = function (x) {
+        socket.send(x);
     };
-    oChessGame.connect = function (url) {
-
+    oSocket.connect = function (url) {
+        if ('WebSocket' in window) {
+            socket = new WebSocket(url);
+        } else {
+            console.log("Sorry, Not Support Socket");
+        }
+        socket.onerror = function () {
+            console.log("error");
+        };
+        socket.onopen = function (event) {
+            console.log("open")
+        };
+        socket.onmessage = function (event) {
+            console.log("收到的消息：" + event)
+        };
+        socket.onclose = function () {
+            console.log("close")
+        };
+        window.onbeforeunload = function () {
+            socket.close();
+        };
+    };
+    oSocket.close = function () {
+        socket.close();
     }
-    return oChessGame;
+    return oSocket;
 };
