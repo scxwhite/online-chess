da6eb68c5cc4bb146c26ab7f405c588390990dc9
xiaosuda <xiaosuda@2dfire.com>
2018-03-19 17:00:11 +0800

    fuck

---
+++ chess/src/main/resources/static/js/socket.js
@@ -0,0 +1,10 @@
+var Socket = function () {
+    var oChessGame = new Object();
+    oChessGame.send = function (x) {
+
+    };
+    oChessGame.connect = function (url) {
+
+    }
+    return oChessGame;
+};
