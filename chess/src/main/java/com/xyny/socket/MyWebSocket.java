3c6861096ceb44f447dc074fe7df5f5d02013929
xiaosdua <1142819049@qq.com>
2018-03-19 22:20:57 +0800

    后端socket

---
+++ chess/src/main/java/com/xyny/socket/MyWebSocket.java
@@ -0,0 +1,33 @@
+package com.xyny.socket;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @author suchengxiang
+ * @version 创建时间：2018/3/19 21:50
+ */
+@ServerEndpoint("/ws/push/")
+public class MyWebSocket {
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("连接成功:" + session);
+    }
+
+    @OnMessage
+    public void onMessage(String msg, Session session) {
+        System.out.println("收到的信息:" + msg);
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+        System.out.println("关闭");
+    }
+
+    @OnError
+    public void onError(Throwable t) {
+        t.printStackTrace();
+    }
+
+}
--- chess/src/main/resources/static/js/socket.js
+++ chess/src/main/resources/static/js/socket.js
@@ -1,6 +1,6 @@
 var Socket = function () {
     var oSocket = new Object();
-    var socket;
+    var socket, isConnect = false;
     oSocket.send = function (x) {
         socket.send(x);
     };
@@ -15,16 +15,22 @@ var Socket = function () {
         };
         socket.onopen = function (event) {
             console.log("open")
+            isConnect = true;
+            socket.send("前端发送的消息");
         };
         socket.onmessage = function (event) {
             console.log("收到的消息：" + event)
         };
         socket.onclose = function () {
             console.log("close")
+            isConnect = false;
         };
         window.onbeforeunload = function () {
             socket.close();
         };
+    };
+    oSocket.isConnect = function () {
+        return isConnect;
     };
     oSocket.close = function () {
         socket.close();
